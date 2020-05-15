0eee5efa19ccf1af4c84ae865e7753552463d7f4
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-05-15 13:06:39 -0300

    Mudança no gradle para ser mais compativel

--- projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/MainActivity.java
+++ projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/MainActivity.java
@@ -2,7 +2,12 @@ package br.edu.insper.al.sophiaks.projeto_minimo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonLogin = findViewById(R.id.button_login);
+        final EditText textUsername = findViewById(R.id.text_username);
+        final EditText textPassword = findViewById(R.id.text_password);
+
+        //          METODO DE TRAÇO
+        buttonLogin.setOnClickListener((view) -> {
+            String username = textUsername.getText().toString();
+            String password = textPassword.getText().toString();
+
+        });
     }
+
+
 }
