50995405ac85a5cc5b51755993800f8a1d7f1d22
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-05-23 17:32:53 -0300

    pequena atualização no main activity para receber o código

--- projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/MainActivity.java
+++ projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/MainActivity.java
@@ -1,7 +1,7 @@
 package br.edu.insper.al.sophiaks.projeto_minimo;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,40 +16,88 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    public String url= "http://sistema.programasemente.com.br/profile/auth_view_mobile/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        CookieManager manager = new CookieManager();
+        CookieHandler.setDefault(manager);
         setContentView(R.layout.activity_main);
         Button buttonLogin = findViewById(R.id.button_login);
         final EditText textUsername = findViewById(R.id.text_username);
         final EditText textPassword = findViewById(R.id.text_password);
-
-        //          METODO DE TRAÇO
+        //          METODO DE Login
         buttonLogin.setOnClickListener((view) -> {
-            String url = "http://sistema.programasemente.com.br/profile/auth_view_mobile/";
             String username = textUsername.getText().toString();
             String password = textPassword.getText().toString();
-            System.out.print("Clicou");
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    if(response.equals("true")){
-                        startActivity(new Intent(MainActivity.this,Home.class));
-                    }
-                    else{
-                        Toast.makeText(getApplicationContext(), "Usuário ou senha incorretos", Toast.LENGTH_LONG).show();
-                    }
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    System.out.print("oops");
-                }
-            });
+            if(validateLogin(username, password)) {
+                //do login
+                doLogin(username, password);
+            }
         });
     }
+    private boolean validateLogin(String username, String password){
+        if(username == null || username.trim().length() == 0){
+            Toast.makeText(this, "Username is required", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(password == null || password.trim().length() == 0){
+            Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
+    private void doLogin(final String username,final String password) {
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams .put("username",username);
+            jsonParams .put("password",password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //getJsonData();
+                        Log.d("Response", response);
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+}
 
