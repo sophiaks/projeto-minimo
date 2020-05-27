295d74b5515a487e798f1f809e0fa51e840b7af7
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-05-26 22:22:53 -0300

    Home para um GET json, não está finalizado o GET ainda

--- projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/Home.java
+++ projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/Home.java
@@ -3,14 +3,95 @@ package br.edu.insper.al.sophiaks.projeto_minimo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class Home extends AppCompatActivity {
+    Dictionary usuario = new Dictionary() {
+        @Override
+        public int size() {
+            return 0;
+        }
 
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public Enumeration keys() {
+            return null;
+        }
+
+        @Override
+        public Enumeration elements() {
+            return null;
+        }
+
+        @Override
+        public Object get(Object key) {
+            return null;
+        }
+
+        @Override
+        public Object put(Object key, Object value) {
+            return null;
+        }
+
+        @Override
+        public Object remove(Object key) {
+            return null;
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+//        String serie_id = usuario.get("id").toString();
+//        String escola_id = usuario.get("escola_id").toString();
+//        String url= "http://sistema.programasemente.com.br/dashboard/index_mobile/"+serie_id+"/"+escola_id+"/";
+        String url= "http://sistema.programasemente.com.br/dashboard/index_mobile/5/54829/";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Response", response.toString());
+                        textView.setText("Response: " + response.toString());
+                        Toast.makeText(Home.this, response., Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
 
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                    }
+                });
 
+        // Access the RequestQueue through your singleton class.
+        //        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
     }
+
 }
