2139a35c35b5981a87d7da0c6cb8faf2b073b19c
Sophia Kerber <sophia.shigueoka@gmail.com>
2020-06-09 17:42:23 -0300

    Validação do código

--- projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/ProfessorRegister.java
+++ projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/ProfessorRegister.java
@@ -3,6 +3,7 @@ package br.edu.insper.al.sophiaks.projeto_minimo;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -18,48 +19,44 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
 
 public class ProfessorRegister extends AppCompatActivity {
     final String LOG_TAG = "myLogs";
-    ListView listaGenero;
     ListView optProfessor;
     String[] names;
     String[] ptypes;
-    String codigo;
-    String postRequest;
-    private Boolean showQuestions;
-    EditText nameProf;
     TextView textOptProf;
-    EditText emailProf;
-    EditText passwordProf;
-    EditText passwordConfProf;
-
-    private void hideKeyboard(EditText editText)
-    {
-        InputMethodManager imm= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor);
-        listaGenero = (ListView) findViewById(R.id.listaGenero);
-        optProfessor = (ListView) findViewById(R.id.optProfessor);
-        nameProf = (EditText) findViewById(R.id.nameProf);
-        textOptProf = (TextView) findViewById(R.id.textOptProf);
-        emailProf = (EditText) findViewById(R.id.emailProf);
-        passwordProf = (EditText) findViewById(R.id.passwordProf);
-        passwordConfProf = (EditText) findViewById(R.id.passwordConfProf);
+        ListView listaGenero = findViewById(R.id.listaGenero);
+        ListView optProfessor = findViewById(R.id.optProfessor);
+        EditText nameProf = findViewById(R.id.nameProf);
+        textOptProf = findViewById(R.id.textOptProf);
+        EditText emailProf = findViewById(R.id.emailProf);
+        EditText passwordProf = findViewById(R.id.passwordProf);
+        EditText passwordConfProf = findViewById(R.id.passwordConfProf);
+        Button btnValid = findViewById(R.id.validBtton);
 
-        // here we adjust list elements choice mode
         listaGenero.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         optProfessor.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         // create adapter using array from resources file
@@ -77,35 +74,48 @@ public class ProfessorRegister extends AppCompatActivity {
         ptypes = getResources().getStringArray(R.array.ptypes);
         final EditText codProf = findViewById(R.id.cod_prof);
         RequestQueue queue = Volley.newRequestQueue(this);
-        codProf.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                    String codProfessor = codProf.getText().toString();
-                    String url = "http://plataformasementedev.minimo.com.br/escolas/api/list?format=json&codigo_professores=" + codProfessor;
-                    StringRequest postRequest = new StringRequest(Request.Method.POST, url, (Response.Listener<String>) response -> {
-                        if (response != null) {
-                            showQuestions = true;
-                            nameProf.setVisibility(View.VISIBLE);
-                            listaGenero.setVisibility(View.VISIBLE);
-                            textOptProf.setVisibility(View.VISIBLE);
-                            optProfessor.setVisibility(View.VISIBLE);
-                            emailProf.setVisibility(View.VISIBLE);
-                            passwordProf.setVisibility(View.VISIBLE);
-                            passwordConfProf.setVisibility(View.VISIBLE);
-                        }
-                        else {
-                        }
-                    } , new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Log.d("VOLLEY", error.toString());
-                            Toast.makeText(ProfessorRegister.this, error.toString(), Toast.LENGTH_LONG).show();
-                        }
-                    });
+        btnValid.setOnClickListener((view) -> {
+                String codProfessor = codProf.getText().toString();
+                String url = "http://plataformasementedev.minimo.com.br/escolas/api/list?format=json&codigo_professores=" + "CODP";
+                JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    if (response != null) {
+                        nameProf.setVisibility(View.VISIBLE);
+                        listaGenero.setVisibility(View.VISIBLE);
+                        textOptProf.setVisibility(View.VISIBLE);
+                        optProfessor.setVisibility(View.VISIBLE);
+                        emailProf.setVisibility(View.VISIBLE);
+                        passwordProf.setVisibility(View.VISIBLE);
+                        passwordConfProf.setVisibility(View.VISIBLE);
+                    } else {
+                        Toast.makeText(ProfessorRegister.this, "Código inválido", Toast.LENGTH_SHORT).show();
+                    }
                 }
-                return false;
             }
+            , new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    String message = null;
+                    if (volleyError instanceof NetworkError) {
+                        message = "Cannot connect to Internet...Please check your connection!";
+                    } else if (volleyError instanceof ServerError) {
+                        message = "The server could not be found. Please try again after some time!!";
+                    } else if (volleyError instanceof AuthFailureError) {
+                        message = "Cannot connect to Internet...Please check your connection!";
+                    } else if (volleyError instanceof ParseError) {
+                        message = "Parsing error! Please try again after some time!!";
+                    } else if (volleyError instanceof NoConnectionError) {
+                        message = "Cannot connect to Internet...Please check your connection!";
+                    } else if (volleyError instanceof TimeoutError) {
+                        message = "Connection TimeOut! Please check your internet connection.";
+                    }
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                }
+
+                });
+            queue.add(getRequest);
         });
     }
 }
+
