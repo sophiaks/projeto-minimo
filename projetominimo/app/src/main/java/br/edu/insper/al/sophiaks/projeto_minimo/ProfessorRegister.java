7500b22419ce67210f6f54a5ccca239a08eb93dc
Sophia Kerber <sophia.shigueoka@gmail.com>
2020-06-09 10:23:14 -0300

    Parte do Back - RegProf

--- projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/ProfessorRegister.java
+++ projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/ProfessorRegister.java
@@ -1,5 +1,6 @@
 package br.edu.insper.al.sophiaks.projeto_minimo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -7,6 +8,7 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +34,18 @@ public class ProfessorRegister extends AppCompatActivity {
     String codigo;
     String postRequest;
     private Boolean showQuestions;
+    EditText nameProf;
+    TextView textOptProf;
+    EditText emailProf;
+    EditText passwordProf;
+    EditText passwordConfProf;
+
+    private void hideKeyboard(EditText editText)
+    {
+        InputMethodManager imm= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +53,12 @@ public class ProfessorRegister extends AppCompatActivity {
         setContentView(R.layout.activity_professor);
         listaGenero = (ListView) findViewById(R.id.listaGenero);
         optProfessor = (ListView) findViewById(R.id.optProfessor);
+        nameProf = (EditText) findViewById(R.id.nameProf);
+        textOptProf = (TextView) findViewById(R.id.textOptProf);
+        emailProf = (EditText) findViewById(R.id.emailProf);
+        passwordProf = (EditText) findViewById(R.id.passwordProf);
+        passwordConfProf = (EditText) findViewById(R.id.passwordConfProf);
+
         // here we adjust list elements choice mode
         listaGenero.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         optProfessor.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
@@ -56,25 +76,36 @@ public class ProfessorRegister extends AppCompatActivity {
         names = getResources().getStringArray(R.array.names);
         ptypes = getResources().getStringArray(R.array.ptypes);
         final EditText codProf = findViewById(R.id.cod_prof);
-        codProf.setOnEditorActionListener((v, actionId, event) -> {
-            String codProfessor = codProf.getText().toString();
-            RequestQueue queue = Volley.newRequestQueue(this);
-            String url = "http://plataformasementedev.minimo.com.br/escolas/api/list?format=json&codigo_professores=" + codProfessor;
-            StringRequest postRequest = new StringRequest(Request.Method.POST, url, (Response.Listener<String>) response -> {
-                if (response != null) {
-                    showQuestions = true;
-                } else {
-                    Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        codProf.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    String codProfessor = codProf.getText().toString();
+                    String url = "http://plataformasementedev.minimo.com.br/escolas/api/list?format=json&codigo_professores=" + codProfessor;
+                    StringRequest postRequest = new StringRequest(Request.Method.POST, url, (Response.Listener<String>) response -> {
+                        if (response != null) {
+                            showQuestions = true;
+                            nameProf.setVisibility(View.VISIBLE);
+                            listaGenero.setVisibility(View.VISIBLE);
+                            textOptProf.setVisibility(View.VISIBLE);
+                            optProfessor.setVisibility(View.VISIBLE);
+                            emailProf.setVisibility(View.VISIBLE);
+                            passwordProf.setVisibility(View.VISIBLE);
+                            passwordConfProf.setVisibility(View.VISIBLE);
+                        }
+                        else {
+                        }
+                    } , new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("VOLLEY", error.toString());
+                            Toast.makeText(ProfessorRegister.this, error.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    });
                 }
+                return false;
             }
-                    , new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.d("VOLLEY", error.toString());
-                    Toast.makeText(ProfessorRegister.this, error.toString(), Toast.LENGTH_LONG).show();
-                }
-            });
-            return false;
         });
     }
 }
