d6b3ecae068e3583438a2ad6449bc319c073cdb5
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-06-12 15:06:12 -0300

    notificações salvas com Shared Preference

--- projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/MyFirebaseMessagingService.java
+++ projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/MyFirebaseMessagingService.java
@@ -1,10 +1,24 @@
 package br.edu.insper.al.sophiaks.projeto_minimo;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
+import androidx.core.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    Set<String> listTitle = new HashSet<String>();
+    Set<String> listBody = new HashSet<String>();
     /**
      * Called if InstanceID token is updated. This may occur if the security of
      * the previous token had been compromised. Note that this is called when the InstanceID token
@@ -21,5 +35,32 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
     private void sendRegistrationToServer(String token) {
         // TODO: Implement this method to send token to your app server.
+    }
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d("TAG", "Message data payload: " + remoteMessage.getData());
+        }
+
+// Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d("Message", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            Log.d("Message", "Message Notification title: " + remoteMessage.getNotification().getTitle());
+            saveNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+    }
+    private void saveNotification(String title, String body) {
+        SharedPreferences sharedPreferences = getSharedPreferences("Notifications", Context.MODE_PRIVATE);
+        // Your notification serialization logic here…
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        listTitle.add(title);
+        listBody.add(body);
+        editor.putString("title", title);
+        editor.putStringSet("listTitle", listTitle);
+        editor.putStringSet("listBody",listBody);
+        editor.apply();
+        Log.d("Message", "Message Notification title set: " + sharedPreferences.getStringSet("listTitle",null));
+
     }
 }
--- projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/Notifications.java
+++ projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/Notifications.java
@@ -1,14 +1,82 @@
 package br.edu.insper.al.sophiaks.projeto_minimo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Notifications extends AppCompatActivity {
-
+    ArrayList<ExampleNotification> mExampleList;
+    TextView testenot;
+    Button buttonnot;
+    SharedPreferences sharedPreferences;
+    Set<String> setTitle;
+    Set<String> setBody;
+    String title;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+        mExampleList = new ArrayList<>();
+        loadNotification();
+        buildRecyclerView();
+        buttonnot = findViewById(R.id.buttonnot);
+        buttonnot.setOnClickListener((view) -> {
+            refreshNotification();
+            buildRecyclerView();
+        });
+    }
+    public void loadNotification(){
+        sharedPreferences = getSharedPreferences("Notifications", Context.MODE_PRIVATE);
+        setTitle = sharedPreferences.getStringSet("listTitle",null);
+        setBody = sharedPreferences.getStringSet("listBody",null);
+        if (setTitle != null){
+            int n = setTitle.size();
+            String[] arraytitle = new String[n];
+            String[] arraybody = new String[n];
+            arraytitle = setTitle.toArray(arraytitle);
+            arraybody = setBody.toArray(arraybody);
+            for (int i = 0; i < arraytitle.length; i++){
+                mExampleList.add(new ExampleNotification(arraytitle[i], arraybody[i]));
+            }
+            sharedPreferences.edit().clear().apply();
+        }
+    }
+    public void refreshNotification(){
+        sharedPreferences = getSharedPreferences("Notifications", Context.MODE_PRIVATE);
+        setTitle = sharedPreferences.getStringSet("listTitle",null);
+        setBody = sharedPreferences.getStringSet("listBody",null);
+        if (setTitle != null){
+            int n = setTitle.size();
+            String[] arraytitle = new String[n];
+            String[] arraybody = new String[n];
+            arraytitle = setTitle.toArray(arraytitle);
+            arraybody = setBody.toArray(arraybody);
+            for (int i = 0; i < arraytitle.length; i++){
+                mExampleList.add(new ExampleNotification(arraytitle[i], arraybody[i]));
+            }
+            sharedPreferences.edit().clear().apply();
+        }
+    }
+
+    public void buildRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new NotificationAdapter(mExampleList);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
