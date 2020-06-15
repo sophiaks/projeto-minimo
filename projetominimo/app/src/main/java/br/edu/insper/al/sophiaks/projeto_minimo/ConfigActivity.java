2f336a6402ea53f1b9223dd371577084e8774611
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-06-15 19:08:39 -0300

    Feito as classes pro config

---
+++ projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/ConfigActivity.java
@@ -0,0 +1,14 @@
+package br.edu.insper.al.sophiaks.projeto_minimo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
