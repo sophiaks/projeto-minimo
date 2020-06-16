7a44e032e51c1033e9d3018a847bd6f0ec14f5a7
Eiki Yamashiro Santos <49211526+EikiSantos@users.noreply.github.com>
2020-06-16 01:41:11 -0300

    Agora printa a serie correta

--- projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/ConfigActivity.java
+++ projetominimo/app/src/main/java/br/edu/insper/al/sophiaks/projeto_minimo/ConfigActivity.java
@@ -25,6 +25,7 @@ public class ConfigActivity extends AppCompatActivity {
     String acesso;
     String username;
     String response;
+    String ano;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,11 +51,57 @@ public class ConfigActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        if (serie.equals("1")){
+            ano = "E.F. II - 6° ano";
+        }
+        if (serie.equals("2")){
+            ano = "E.F. II - 7° ano";
+        }
+        if (serie.equals("3")){
+            ano = "E.F. II - 8° ano";
+        }
+        if (serie.equals("4")){
+            ano = "E.F. II - 9° ano";
+        }
+        if (serie.equals("5")){
+            ano = "E.F. I - 1° ano";
+        }
+        else if (serie.equals("6")){
+            ano = "E.F. I - 2° ano";
+        }
+        else if (serie.equals("7")){
+            ano = "E.F. I - 3° ano";
+        }
+        else if (serie.equals("8")){
+            ano = "E.F. I - 4° ano";
+        }
+        else if (serie.equals("9")){
+            ano = "E.F. I - 5° ano";
+        }
+        else if (serie.equals("10")){
+            ano = "E.M. - 1ª Série";
+        }
+        else if (serie.equals("11")){
+            ano = "E.M. - 2ª Série";
+        }
+        else if (serie.equals("12")){
+            ano = "E.M. - 3ª Série";
+        }
+        else if (serie.equals("19")){
+            ano = "E.I. - 4 anos";
+        }
+        else if (serie.equals("20")){
+            ano = "E.I. - 5 anos";
+        }
+
+
+
         Log.d("id", serie + " " + acesso);
         Log.d("id",username);
         configUsername.setText(username);
         configAcesso.setText(acesso);
-        configSerie.setText(serie);
+        configSerie.setText(ano);
 
         ImageButton back = findViewById(R.id.back);
 
